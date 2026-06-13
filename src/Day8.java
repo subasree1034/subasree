2e4c40a79d770484233c924fdf2767c2e6f16ec1
subasree1034 <subasreee9@gmail.com>
2026-06-13 14:22:47 +0530

    language method

--- src/Day8.java
+++ src/Day8.java
@@ -1,4 +1,4 @@
-public class Day8{
+/*public class Day8{
     record CourseRecord(String name, String dept) {
 
     }
@@ -8,6 +8,29 @@ public class Day8{
         System.out.println(courserecord.name());
         System.out.println(courserecord.dept());
 
+    }
+}
+*/
+public class Day8 {
+    enum LANGUAGE {
+        EN("English"),
+        GN("German"),
+        JP("Japanese"),
+        TN("Tamil");
+
+
+        String description;
+
+        LANGUAGE(String description) {
+            this.description = description;
+        }
+    }
+
+    static void main() {
+        System.out.println(LANGUAGE.EN.description);
+        System.out.println(LANGUAGE.GN.description);
+        System.out.println(LANGUAGE.JP.description);
+        System.out.println(LANGUAGE.TN.description);
     }
 }
 
