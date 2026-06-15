593d078a4aa5d657020b47e8f7456e4424a63178
subasree1034 <subasreee9@gmail.com>
2026-06-15 09:37:42 +0530

    sealed method

--- src/Day8.java
+++ src/Day8.java
@@ -11,6 +11,7 @@
     }
 }
 */
+/*
 public class Day8 {
     enum LANGUAGE {
         EN("English"),
@@ -33,4 +34,52 @@ public class Day8 {
         System.out.println(LANGUAGE.TN.description);
     }
 }
+*/
+public class Day8_P2 {
+
+    // Login
+    sealed interface LoginResult
+            permits LoginSuccess, LoginFailure, LoginLoading {
+    }
+
+    record LoginSuccess(String username) implements LoginResult {
+    }
+
+    record LoginFailure(String reason) implements LoginResult {
+    }
+
+    record LoginLoading(String message) implements LoginResult {
+    }
+
+    static void handleLogin(LoginResult result) {
+        switch (result) {
+            case LoginSuccess success ->
+                    System.out.println("Login Successful : " + success.username());
+
+            case LoginFailure failure ->
+                    System.out.println("Login Failed because of " + failure.reason());
+
+            case LoginLoading loading ->
+                    System.out.println("Login Loading... " + loading.message());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LoginResult loginSuccess =
+                new LoginSuccess("Joseph");
+
+        LoginResult loginFailure =
+                new LoginFailure("Invalid Password");
+
+        LoginResult loginLoading =
+                new LoginLoading("Please wait...");
+
+        handleLogin(loginSuccess);
+        handleLogin(loginFailure);
+        handleLogin(loginLoading);
+    }
+}
+
+
 
