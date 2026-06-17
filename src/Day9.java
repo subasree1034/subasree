69145918750729194f2c39f2ff360d3f8c085cb6
subasree <subasreee9@gmail.com>
2026-06-17 09:46:29 +0530

    students linkedlist method

--- src/Day9.java
+++ src/Day9.java
@@ -1,1 +1,19 @@
-..
+import java.util.LinkedList;
+public class day10 {
+    static void main() {
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("joseph");
+        queue.add("subasree");
+        queue.add("srinithi");
+        queue.add("archana");
+        queue.add("deva");
+        while(!queue.isEmpty()) {
+            process(queue);
+        }
+    }
+    static void process(LinkedList<String> queue){
+        String removed=queue.removeFirst();
+        System.out.println("Removed:"+removed);
+
+    }
+}
