109f7261799f149cd6d1101500602864e699a3f0
subasree <subasreee9@gmail.com>
2026-06-22 14:59:55 +0530

    first commit

--- src/Day14.java
+++ src/Day14.java
@@ -48,6 +48,40 @@
         System.out.println(student.regno);
     }
 }
+*/
 
- */
+public class Day14_Factory {
+    interface Notification {
+        void send(String to, String message);
+    }
 
+    class EmailNotification implements Notification{
+
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Email: "+message +", to "+to);
+        }
+    }
+    class WhatsappNotification implements Notification{
+
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Whatsapp: "+message +", to "+to);
+        }
+    }
+
+    static Notification getNotification(Day14_Factory factory, String type){
+        return switch (type){
+            case "Email"-> factory.new EmailNotification();
+            case "Whatsapp"-> factory.new WhatsappNotification();
+            default -> throw new IllegalArgumentException("Invalid notification type: "+type);
+        };
+    }
+
+    static void main() {
+        Day14_Factory factory = new Day14_Factory();
+
+        String type = "Whatsapp";
+        getNotification(factory, type).send("56784","Hello World");
+    }
+}
